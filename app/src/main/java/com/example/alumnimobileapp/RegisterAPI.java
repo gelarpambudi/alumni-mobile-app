b5db13e84533e893f598d0796abb138f6a8b6f5b
Gelar Pambudi <gelargelur@gmail.com>
2020-10-09 19:32:38 +0700

    add RegisterAPI class

---
+++ app/src/main/java/com/example/alumnimobileapp/RegisterAPI.java
@@ -0,0 +1,18 @@
+package com.example.alumnimobileapp;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public class RegisterAPI {
+
+    @FormUrlEncoded
+    //TODO Add insert API
+    @POST("/insert.php")
+    Call<Value> daftar(@Field("Nama") String nama,
+                       @Field("Angkatan") String angkatan,
+                       @Field("Nomor_HP") String no_hp,
+                       @Field("Email") String email);
+
+}
